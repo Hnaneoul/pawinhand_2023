52b24f0a9428a12f94cd700d2f72c4e4f7640371
LJK <dlworhkd987@naver.com>
2023-07-28 17:36:49 +0900

    swiper

--- script/script.js
+++ script/script.js
@@ -28,3 +28,33 @@ m_nav.addEventListener('click',function(){
 close.addEventListener('click',function(){
     m_nav_open.style.display = 'none' //3-2 모바일 메뉴 숨기기
 })
+
+// main-swiper-slide
+// const 변수명 = new Swiper ('적용대상')
+// const 변수명 = new Swiper ('적용대상',{속성:값 , 속성:값})
+const pawin_slide = new Swiper('#pawin_slide',{
+    autoplay:{
+        delay:1000, //슬라이드 간격(밀리초)기본3초
+        disableOnInteraction:false, //버튼 클릭 후 자동재생 유지하는 방법
+    },  //자동재생 1000=1초
+    loop:true ,  // 마지막장에서 자연스럽게 1번 넘기기
+    effect:'fade', // 제자리 변경 슬라이드 효과
+    navigation: { 
+        //next, prev 객체 연결시 부모를 안적으면
+        // body안에 있는 모든 swiper-next,prev를 인식하기 때문에
+        //개별인식 가능한 부모이름을 반드시 앞에 먼저 작성한다.
+        nextEl: '#pawin_slide .swiper-button-next',
+        prevEl: '#pawin_slide .swiper-button-prev',
+    }, //이전,다음버튼 활성화
+
+    // direction : 'vertical' 위아래 슬라이드
+});
+const s = new Swiper('#s',{
+    autoplay:{delay:1000},
+    loop:true,
+    direction : 'vertical',
+    navigation: { 
+        nextEl: '#s .swiper-button-next',
+        prevEl: '#s .swiper-button-prev',
+    }
+})
